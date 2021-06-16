c54423b417473bf272d63403c9c9498016b49323
Clément van Staen <clement.vanstaen@gmail.com>
2021-06-16 12:08:42 +0200

    backend WIP

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 5012;
+require("dotenv/config");
+
+// Init Express
+const app = express();
+
+// Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Allow cross origin request
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
+// Set up for React
+app.use(express.static(path.join(__dirname, "build")));
+app.get('/', (req, res) => { res.sendFile(path.join(__dirname, "build", "index.html")); });
+
+// Listen on a port
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
