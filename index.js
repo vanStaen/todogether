441c89890269b7b2dcaef7d574064dde4c37dfe9
Clément van Staen <clement.vanstaen@gmail.com>
2021-06-22 21:55:38 +0200

    move connection function ot other folder

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const path = require("path");
-const http = require("http");
 const PORT = process.env.PORT || 5012;
-const { Sequelize } = require("sequelize");
-require("dotenv/config");
+require('./lib/connectToDb');
 
 // Init Express
 const app = express();
@@ -33,26 +31,4 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 // Connect to db with sequelize
-const connectToDb = async () => {
-  const sequelize = new Sequelize({
-    database: process.env.DATABASE_NAME,
-    username: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PWD,
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
-  });
-  try {
-    await sequelize.authenticate();
-    console.log("Connection has been established successfully.");
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-  }
-};
 connectToDb();
