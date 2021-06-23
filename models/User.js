b06b79958e11bcdbaa272e83235ddda109f2855b
Clément van Staen <clement.vanstaen@gmail.com>
2021-06-23 13:53:49 +0200

    define User model

--- models/User.js
+++ models/User.js
@@ -1,33 +1,57 @@
-const { Sequelize, DataTypes } = require('sequelize');
+const { Sequelize, DataTypes } = require("sequelize");
 require("dotenv/config");
 
 const sequelize = new Sequelize({
-    database: process.env.DATABASE_NAME,
-    username: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PWD,
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
+  database: process.env.DATABASE_NAME,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PWD,
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
     },
-  });
-
-const User = sequelize.define('User', {
-  // Model attributes are defined here
-  firstName: {
-    type: DataTypes.STRING,
-    allowNull: false
   },
-  lastName: {
-    type: DataTypes.STRING
-  }
-}, {
-  // Other model options go here
 });
+
+const User = sequelize.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    avatar: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    categories: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    emailSettings: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    displaySettings: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    // Other model options go here
+  }
+);
 
 // `sequelize.define` also returns the model
 console.log(User === sequelize.models.User);
