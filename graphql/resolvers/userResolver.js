c08b5723315a3680557b4662edb1d509c4fb5fd0
vanStaen <clement.vanstaen@gmail.com>
2021-07-15 15:59:36 +0200

    user resolver WIP

--- graphql/resolvers/userResolver.js
+++ graphql/resolvers/userResolver.js
@@ -2,58 +2,49 @@ const bcrypt = require("bcryptjs");
 const User = require("../../models/User");
 
 exports.User = {
-  async user (args, req) {
+  async user(args, req) {
     // const users = await User.find({ id: args.id });
     const users = await User.findAll();
-    return users
+    return users;
   },
 
   // addUser(userInput: UserInputData!): User!
-  // rewrite with AWAITs
-  async createUser (args, req) {
-    return User.findOne({ email: args.userInput.email })
-      .then((user) => {
-        if (user) {
-          throw new Error("This email is already associated with an account.");
-        }
-        return bcrypt.hash(args.userInput.password, 12);
-      })
-      .then((hashedPassword) => {
-        const user = new User({
-          name: args.userInput.name,
-          email: args.userInput.email,
-          password: hashedPassword,
-          emailSettings: "[]",
-          displaySettings: "[]",
-        });
-        return user.save();
-      })
-      .then((result) => {
-        return { ...result, password: null };
-      })
-      .catch((err) => {
-        throw err;
-      });
+  async createUser(args, req) {
+    console.log("createdUser started");
+    
+    const foundUser = await User.findOne({ email: args.userInput.email });
+    if (foundUser) {
+      throw new Error("This email is already associated with an account.");
+    } 
+    hashedPassword = await bcrypt.hash(args.userInput.password, 12);
+    const user = new User({
+      name: args.userInput.name,
+      email: args.userInput.email,
+      password: hashedPassword,
+      emailSettings: "[]",
+      displaySettings: "[]",
+    });
+    const result = user.save();
+    console.log("result", result);
+    return { ...result, password: null };
   },
 
   // updateUser(id: ID!, userInput: UserInputData!): User!
-  async updateUser (args, req) {
+  async updateUser(args, req) {
     //if (!req.isAuth) {
     //  throw new Error(errorName.UNAUTHORIZED);
     //}
 
-    // prepare array of strings with new parameter
-    // go through for each array infos and see if 
-
     const updateField = {};
+    if (args.userInput.password) {
+      updateField.password = await bcrypt.hash(args.userInput.password, 12);
+    }
+
     if (args.userInput.name) {
       updateField.name = args.userInput.name;
     }
     if (args.userInput.email) {
       updateField.email = args.userInput.email;
-    }
-    if (args.userInput.password) {
-      updateField.password = await bcrypt.hash(args.userInput.password, 12);
     }
     if (args.userInput.avatar) {
       updateField.avatar = args.userInput.avatar;
@@ -75,12 +66,12 @@ exports.User = {
   },
 
   // deleteUser(id: ID!): Boolean!
-  async deleteUser  (rgs, req) {
+  async deleteUser(rgs, req) {
     await User.destroy({
       where: {
-        id: args.id
-      }
-    });    
-    return true
+        id: args.id,
+      },
+    });
+    return true;
   },
 };
