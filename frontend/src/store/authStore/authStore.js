6f5b47fda6fa596203dea389ad15b7896a98081f
vanStaen <clement.vanstaen@gmail.com>
2021-07-29 21:59:30 +0200

    authStore way lighter

--- frontend/src/store/authStore/authStore.js
+++ frontend/src/store/authStore/authStore.js
@@ -1,60 +1,38 @@
 import { action, makeObservable, observable } from "mobx";
 
 import { deleteLogout } from "./deleteLogout";
-import { postLoginToken } from "./postLoginToken";
+import { postLogin } from "./postLogin";
 
 export class AuthStore {
-  token = null;
-  refreshToken = localStorage.getItem("refreshToken");
   hasAccess = false;
 
   constructor() {
     makeObservable(this, {
-      token: observable,
-      setToken: action,
-      refreshToken: observable,
-      setRefreshToken: action,
+      login: action,
       logout: action,
       hasAccess: observable,
       setHasAccess: action,
-      getNewToken: action,
     });
   }
 
-  setToken = (token) => {
-    this.token = token;
-  };
-
-  setRefreshToken = (refreshToken) => {
-    this.refreshToken = refreshToken;
-    localStorage.setItem("refreshToken", refreshToken);
+  login = async () => {
+    // Call login endpoint
+    const resultLogIn = await postLogin();
+    if (resultLogIn) {
+      this.hasAccess = true;
+    }
   };
 
   logout = async () => {
-    // Delete refreshtoken from localstorage,
-    localStorage.removeItem("refreshToken");
-    localStorage.removeItem("userId");
-    localStorage.removeItem("user");
-    localStorage.clear();
-    authStore.setToken(null);
-    authStore.setRefreshToken(null);
-    await deleteLogout();
+    // Call logout endpoint
+     const resultLogOut = await deleteLogout();
+     if (resultLogOut) {
+       this.hasAccess = false;
+     }
   };
 
   setHasAccess = (hasAccess) => {
     this.hasAccess = hasAccess;
-  };
-
-  getNewToken = async () => {
-    try {
-      const newToken = await postLoginToken(authStore.refreshToken);
-      authStore.setToken(newToken.data.token);
-      authStore.setHasAccess(true);
-      return newToken;
-    } catch (e) {
-      console.log(e, authStore.refreshToken);
-      // authStore.logout();
-    }
   };
 }
 
