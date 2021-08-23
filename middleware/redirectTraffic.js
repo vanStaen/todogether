a25d26906f3656a98072bbaf209ffa857f2f615c
vanStaen <clement.vanstaen@gmail.com>
2021-08-23 18:52:26 +0200

    traffic redirect

--- middleware/redirectTraffic.js
+++ middleware/redirectTraffic.js
@@ -1,7 +1,12 @@
 module.exports = function redirectTraffic(req, res, next) {
-  if (req.headers.host.slice(0, 4) === "www.") {
-    var newHost = req.headers.host.slice(4);
-    return res.redirect(301, req.protocol + "://" + newHost + req.originalUrl);
+  if (req.headers.host.substring(0, 9) !== "localhost") {
+    if (req.headers.host.slice(0, 4) === "www.") {
+      var newHost = req.headers.host.slice(4);
+      return res.redirect(301, "https://" + newHost + req.originalUrl);
+    }
+    if (req.protocol === "http") {
+      return res.redirect(301, "https://" + req.headers.host + req.originalUrl);
+    }
   }
   next();
 };
