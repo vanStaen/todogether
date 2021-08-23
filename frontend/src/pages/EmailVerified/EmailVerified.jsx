0c8095cf4a438b523583e88d42dd4d8be9696ec7
vanStaen <clement.vanstaen@gmail.com>
2021-08-23 19:30:08 +0200

    email verify translate

--- frontend/src/pages/EmailVerified/EmailVerified.jsx
+++ frontend/src/pages/EmailVerified/EmailVerified.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState, useCallback } from "react";
 import { LoadingOutlined } from "@ant-design/icons";
+import { useTranslation } from "react-i18next";
 
 import { postEmailVerified } from "./postEmailVerified";
+import { LanguageDropDown } from "../../components/LanguageDropDown/LanguageDropDown";
 
 import "./EmailVerified.css";
 
 export const EmailVerified = (props) => {
   const [isLoading, setIsLoading] = useState(true);
   const [isVerified, setIsVerified] = useState(false);
+  const { t } = useTranslation();
 
   const emailIsVerified = useCallback(async () => {
     const success = await postEmailVerified(props.match.params.verifyCode);
@@ -26,19 +29,22 @@ export const EmailVerified = (props) => {
 
   return (
     <div>
-      <div className="emailVerified__leftPanel"></div>
+      <div className="emailVerified__leftPanel">
+        <LanguageDropDown />
+      </div>
       <div className="emailVerified__rightPanel">
         <div className="emailVerified__container">
           {isLoading ? (
             <LoadingOutlined className="emailVerified__loader" />
           ) : isVerified ? (
             <>
-              <b>Your email have been verified</b> <br />
-              Thank you and welcome in our community! <br />
-              You can now go ahead and log yourself in. <br />
+              <strong>{t("login.emailVerified")}</strong> <br />
+              {t("login.welcomeInOurCommunity")}!<br />
+              {t("login.goAheadAndLogin")}.
+              <br />
               <br />
               <div className="emailVerified__link">
-                You will be redirected to the{" "}
+                {t("login.redirectedToThe")}{" "}
                 <span
                   className="link"
                   onClick={() => {
@@ -46,20 +52,19 @@ export const EmailVerified = (props) => {
                   }}
                 >
                   {" "}
-                  login page
+                  {t("login.loginPage")}.
                 </span>
                 .
               </div>
             </>
           ) : (
             <>
-              <strong>Your email could not be verified!</strong>
+              <strong>{t("login.emailNotVerified")}!</strong>
               <br />
-              Something went wrong in the verification of the email linked to
-              your account.
+              {t("login.somethingWrongEmail")}!
               <br />
               <div className="emailVerified__link">
-                What can you do? Go back to the
+                {t("login.whatCanYouDo")}
                 <span
                   className="link"
                   onClick={() => {
@@ -67,10 +72,10 @@ export const EmailVerified = (props) => {
                   }}
                 >
                   {" "}
-                  login page
+                  {t("login.loginPage")}
                 </span>
-                , sign in, and follow the infos on how to request a new
-                verification link.
+                {", "}
+                {t("login.requestNewLink")}.
               </div>
             </>
           )}
