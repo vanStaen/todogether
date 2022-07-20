1ac908cb119d4ee7ff95e1a99e04b3653032fe3a
vanStaen <clement.vanstaen@gmail.com>
2022-07-20 23:17:44 +0200

    Home WIP

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -6,6 +6,11 @@ import "./Home.css";
 export const Home = () => {
   return (
     <div>
+      <div className="home__container">
+        <div className="home__main">
+          <div className="home__header">Todogether</div>
+        </div>
+      </div>
       <div className="home__logout" onClick={authStore.logout}>
         logout
       </div>
