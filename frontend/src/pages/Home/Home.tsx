7a6795a2b1d77812ca06306ed8ce76ace7abd2f2
vanStaen <clement.vanstaen@gmail.com>
2022-07-20 23:52:01 +0200

    moreList WIP

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -10,23 +10,40 @@ export const Home = () => {
     <div>
       <div className="home__container">
         <div className="home__main">
-          <div className="home__header">
+          <div className="home__header home__rowNoBar">
             <div className="home__rowLogo">
               <UnorderedListOutlined />
             </div>
             Todogether
           </div>
-          <div className="home__row">
+          <div className="home__row home__rowNoBar">
             <div className="home__rowLogo">
-              <input type="checkbox" className="home__checkbox" />
+              <input type="checkbox" checked className="home__checkbox" />
             </div>
             First row
           </div>
-          <div className="home__row">
+          <div className="home__row home__rowGoldBar">
             <div className="home__rowLogo">
               <input type="checkbox" className="home__checkbox" />
             </div>
             Second row
+          </div>
+          <div className="home__row home__rowGoldBar">
+            <div className="home__rowLogo">
+              <input type="checkbox" className="home__checkbox" />
+            </div>
+            Third row
+          </div>
+          <div className="home__row home__rowNoBar">
+            <div className="home__rowLogo">
+              <input type="checkbox" checked className="home__checkbox" />
+            </div>
+            fourth row
+          </div><div className="home__row home__rowNoBar">
+            <div className="home__rowLogo">
+              <input type="checkbox" checked className="home__checkbox" />
+            </div>
+            fifth row
           </div>
         </div>
         <div className="home__logout" onClick={authStore.logout}>
