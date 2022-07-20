63fe4b3fc32809e15f799c9d5839b91d5abd04a3
vanStaen <clement.vanstaen@gmail.com>
2022-07-20 23:37:53 +0200

    more WIP on Home

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { UnorderedListOutlined } from "@ant-design/icons";
+
 import { authStore } from "../../stores/authStore/authStore";
 
 import "./Home.css";
@@ -8,11 +10,28 @@ export const Home = () => {
     <div>
       <div className="home__container">
         <div className="home__main">
-          <div className="home__header">Todogether</div>
+          <div className="home__header">
+            <div className="home__rowLogo">
+              <UnorderedListOutlined />
+            </div>
+            Todogether
+          </div>
+          <div className="home__row">
+            <div className="home__rowLogo">
+              <input type="checkbox" className="home__checkbox" />
+            </div>
+            First row
+          </div>
+          <div className="home__row">
+            <div className="home__rowLogo">
+              <input type="checkbox" className="home__checkbox" />
+            </div>
+            Second row
+          </div>
         </div>
-      </div>
-      <div className="home__logout" onClick={authStore.logout}>
-        logout
+        <div className="home__logout" onClick={authStore.logout}>
+          logout
+        </div>
       </div>
     </div>
   );
