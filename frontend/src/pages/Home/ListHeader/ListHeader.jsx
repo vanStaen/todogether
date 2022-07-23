d648ec8f767dec467dd9b715f1c2d7f4188e1385
vanStaen <clement.vanstaen@gmail.com>
2022-07-23 17:48:50 +0200

    header action UX

--- frontend/src/pages/Home/ListHeader/ListHeader.jsx
+++ frontend/src/pages/Home/ListHeader/ListHeader.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { Radio } from 'antd';
+import { Radio, Tooltip } from 'antd';
 import { observer } from "mobx-react";
-import { UnorderedListOutlined } from "@ant-design/icons";
+import { UnorderedListOutlined, DatabaseOutlined, AppstoreOutlined, CheckSquareOutlined, CloseSquareOutlined } from "@ant-design/icons";
 
 import { listStore } from "../../../stores/listStore/listStore";
 
@@ -10,16 +10,32 @@ import "./ListHeader.css";
 export const ListHeader = observer(() => {
   return (
     <div className="listHeader">
-    <div className="listHeader__rowLogo">
-        <UnorderedListOutlined />
-    </div>
-    My shared todolist
-    <div className="listHeader__SwitchShowDone">
-        <Radio.Group defaultValue={listStore.showCompleted} buttonStyle="solid">
-            <Radio.Button value={true}>Show completed</Radio.Button>
-            <Radio.Button value={false}>Hide completed</Radio.Button>
-        </Radio.Group>
-    </div>
+      <div className="listHeader__ListNameContainer">
+        <Tooltip title="Switch between list">
+          <div className="listHeader__rowLogo">
+            <UnorderedListOutlined />
+          </div>
+        </Tooltip>
+        My shared todolist
+      </div>
+      <div className="listHeader__SwitchContainer">
+        <Tooltip title="Change display">
+          <div className="listHeader__Switch">
+            <Radio.Group defaultValue={listStore.displayAslist} size="small" buttonStyle="solid">
+              <Radio.Button value={true}><DatabaseOutlined /></Radio.Button>
+              <Radio.Button value={false}><AppstoreOutlined /></Radio.Button>
+            </Radio.Group>
+          </div>
+        </Tooltip>
+        <Tooltip title={listStore.showCompleted ? "Hide completed task" : "Show completed task"}>
+          <div className="listHeader__Switch">
+            <Radio.Group defaultValue={listStore.showCompleted} size="small" buttonStyle="solid">
+              <Radio.Button value={true}><CheckSquareOutlined /></Radio.Button>
+              <Radio.Button value={false}><CloseSquareOutlined /></Radio.Button>
+            </Radio.Group>
+          </div>
+        </Tooltip>
+      </div>
     </div>
   );
 });
--- frontend/src/stores/listStore/listStore.js
+++ frontend/src/stores/listStore/listStore.js
@@ -2,16 +2,23 @@ import { action, makeObservable, observable } from "mobx";
 
 export class ListStore {
   showCompleted = false;
+  displayAslist = true;
 
   constructor() {
     makeObservable(this, {
       showCompleted: observable,
       setShowCompleted: action,
+      displayAslist: observable,
+      setDisplayAslist: action,
     });
   }
 
   setShowCompleted = (showCompleted) => {
     this.showCompleted = showCompleted;
+  };
+
+  setDisplayAslist = (displayAslist) => {
+    this.displayAslist = displayAslist;
   };
 }
 
