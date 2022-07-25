97b6d4eebffb9fdae809fd4cf44b29e43ccee38f
vanStaen <clement.vanstaen@gmail.com>
2022-07-25 10:09:42 +0200

    overflow main list div

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -14,11 +14,13 @@ export const Home = () => {
       <Header/>
         <div className="home__main">
           <ListHeader/>
-          <ListRow name="fist row" completed={true} selected={true}/>
-          <ListRow name="second row" completed={true} selected={true}/>
-          <ListRow name="third row" completed={false} selected={true}/>
-          <ListRow name="fourth row" completed={false} selected={false}/>
-          <ListRow name="fifth row" completed={false} selected={false}/>
+          <div className="home__rows">
+            <ListRow name="fist row" completed={true} selected={true}/>
+            <ListRow name="second row" completed={true} selected={true}/>
+            <ListRow name="third row" completed={false} selected={true}/>
+            <ListRow name="fourth row" completed={false} selected={false}/>
+            <ListRow name="fifth row" completed={false} selected={false}/>
+          </div>
           <ListFooter/>
         </div>
       </div>
