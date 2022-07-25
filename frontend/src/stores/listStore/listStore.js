2f6f23fb6875863d8784dbc1d1ff169a87a1b37b
vanStaen <clement.vanstaen@gmail.com>
2022-07-25 14:53:53 +0200

    listStore 'selected' functions

--- frontend/src/stores/listStore/listStore.js
+++ frontend/src/stores/listStore/listStore.js
@@ -3,6 +3,8 @@ import { action, makeObservable, observable } from "mobx";
 export class ListStore {
   showCompleted = false;
   displayAslist = true;
+  selectedTasks = [];
+  taskInEditMode = null;
 
   constructor() {
     makeObservable(this, {
@@ -10,6 +12,11 @@ export class ListStore {
       setShowCompleted: action,
       displayAslist: observable,
       setDisplayAslist: action,
+      selectedTasks: observable,
+      selectTask: action,
+      unselectTask: action,
+      taskInEditMode: observable,
+      setTaskInEditMode: action,
     });
   }
 
@@ -20,6 +27,25 @@ export class ListStore {
   setDisplayAslist = (displayAslist) => {
     this.displayAslist = displayAslist;
   };
+
+  selectTask = (task) => {
+    this.selectedTasks.push(task);
+  }
+
+  unselectTask = (task) => {
+    const index = this.selectedTasks.indexOf(task)
+    if (index > 0) {
+      this.selectedTasks.splice(index, 1);
+    }
+    else {
+      console.log("Error", "the task was not in the 'selectedTasks'-array")
+    }
+  }
+
+  setTaskInEditMode = (taskInEditMode) => {
+    this.taskInEditMode = (taskInEditMode);
+  }
+
 }
 
 export const listStore = new ListStore();
