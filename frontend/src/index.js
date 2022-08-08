ae843b9656021ea1f1154f94b6fedc00b1559e43
vanStaen <clement.vanstaen@gmail.com>
2022-08-08 13:37:59 +0200

    service worker WIP

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import '../src/lib/i18n';
 
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("./sw.js").then(() => {
+    console.log("Service worker Registered");
+  });
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
--- frontend/src/sw.js
+++ frontend/src/sw.js
@@ -1,4 +1,5 @@
 self.addEventListener('install', (e) => {
+  console.log("Service worker installed");
   /*e.waitUntil(
     caches.open('todogether').then((cache) => cache.addAll([
       './',
@@ -8,7 +9,12 @@ self.addEventListener('install', (e) => {
   );*/
 });
 
+self.addEventListener("activate", event => {
+  console.log("Service worker activated");
+});
+
 self.addEventListener('fetch', (e) => {
+  console.log("Service worker detected a fetch call");
   /*console.log(e.request.url);
   e.respondWith(
     caches.match(e.request).then((response) => response || fetch(e.request)),
