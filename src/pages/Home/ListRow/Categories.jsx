c445ec79a51dc5f3f4afaf3248eb96e6f616b8f9
vanStaen <clement.vanstaen@gmail.com>
2025-01-17 18:20:54 +0100

    categorie color picker

--- src/pages/Home/ListRow/Categories.jsx
+++ src/pages/Home/ListRow/Categories.jsx
@@ -1,15 +1,21 @@
-import React, { useEffect } from "react";
-import { Input } from "antd";
+import React from "react";
+import { Input, ColorPicker } from "antd";
 import { userStore } from "../../../stores/userStore/userStore";
 
-export const getMenuCategories = () => {
+import "./Categories.less";
 
+export const getMenuCategories = () => {
     const categoriesAsListItem = userStore.categories?.map((categorie, index) => {
         return {
             key: index,
             label: (
-                <div onClick={(e) => e.stopPropagation()}>
-                    {categorie.title}
+                <div onClick={(e) => e.stopPropagation()} className="menuItemCategorie__Container">
+                    <div className="menuItemCategorie__Title">
+                        {categorie.title}
+                    </div>
+                    <div className="menuItemCategorie__ColorPicker">
+                        <ColorPicker defaultValue={categorie.color} size="small" />
+                    </div>
                 </div>
             ),
         }
